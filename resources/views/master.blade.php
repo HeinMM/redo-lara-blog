ebdc3415bd3b6aa3d6f901b8d23f1539f7c74537
HEIN <aungmyat6070@gmail.com>
2022-07-17 04:34:11 +0900

    finish and prepare befor comment and user profile photo

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
 </head>
 <body>
 
@@ -13,6 +13,6 @@
             @yield('content')
         </section>
 
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/theme.js') }}"></script>
 </body>
 </html>
